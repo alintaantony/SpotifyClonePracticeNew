9a3618df6c66e8bfd011abad56f2b61e780c1b52
Alint.Antony <Alint.Antony@KANINI-LTP-327>
2021-08-25 10:57:36 +0530

    started with navbar

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SidebarDash from "./SidebarDash";
+
+const Dashboard = () => {
+  return (
+    <React.Fragment>
+      <SidebarDash />
+    </React.Fragment>
+  );
+};
+
+export default Dashboard;
