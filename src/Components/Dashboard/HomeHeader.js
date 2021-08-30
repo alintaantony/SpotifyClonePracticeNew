69cb06a21ae65f4936501d9769c761e9240b25e7
Manali Jain <Manali.Jain@kanini.com>
2021-08-30 15:35:00 +0530

    added header changes

--- src/Components/Dashboard/HomeHeader.js
+++ src/Components/Dashboard/HomeHeader.js
@@ -4,13 +4,29 @@ import UserIcon from '../../Images/UserIcon1.jpg';
 
 const HomeHeader=()=>{
     return(
+        <div className={stylesForHeader.MainContainer}>
         <div className={stylesForHeader.HeaderContainer}>
-            <button className={stylesForHeader.dropdownButton}>Manali</button>
+            
+            <button className={stylesForHeader.dropdownButton}>
+                
+                    <span>
+                        <img src={UserIcon} alt="User Icon image"/>
+                    </span>
+                    <span><p>Manali</p></span>
+                
+            </button>
+           
                 <div className={stylesForHeader.dropdownContent}>
                 <a href="#"><b>Profile</b></a>
                 <a href="#"><b>Log out</b></a>
                 </div>
         </div>
+        <div className={stylesForHeader.HomeBodyContainer}>
+            <h1>Good Afternoon</h1>
+        </div>
+        </div>
+        
+
     );
 }
 
