9da1169ec7fee05683713523b1f8be36b479afa2
Manali Jain <Manali.Jain@kanini.com>
2021-08-30 12:48:19 +0530

    added css

---
+++ src/Components/Dashboard/HomeDb.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HomeHeader from "./HomeHeader";
+const HomeDb=()=>{
+    return(
+       <React.Fragment >
+            <HomeHeader/>
+          
+        </React.Fragment>
+    );
+}
+
+export default HomeDb;
