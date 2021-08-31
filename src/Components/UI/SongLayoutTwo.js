189641c1aa5228e5160a0be30b7f45b59c903b29
Alint.Antony <Alint.Antony@KANINI-LTP-327>
2021-08-31 21:22:25 +0530

    Added SongLayoutTwo

--- src/Components/UI/SongLayoutTwo.js
+++ src/Components/UI/SongLayoutTwo.js
@@ -1,5 +1,20 @@
-const SongLayoutTwo = () => {
-  return <div></div>;
+import stylesForSLT from "./SongLayoutTwo.module.css";
+
+const SongLayoutTwo = (props) => {
+  return (
+    <div className={stylesForSLT.mainContainer}>
+      <div classname={stylesForSLT.container}>
+        <ul>
+          <li>
+            <img src={props.src} alt={props.alt} />
+          </li>
+          <li>
+            <h2>{props.title}</h2>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default SongLayoutTwo;
